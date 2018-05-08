38154c70be608b90cdd5a4843781ac3d154c1035
Andrew Redfern <redfern_uk@hotmail.com>
2018-05-08 15:33:22 +0100

    Boditrax-Prep-For-Calculator-Project
    Boditrax-Prep-For-Calculator-Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Prep_For_Calculator_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "5656";
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(119, 61);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(112, 27);
+            this.btnAnswer.TabIndex = 1;
+            this.btnAnswer.Text = "Answer";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "7";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 24);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "2156";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 211);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prep_For_Calculator_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            int firstTextBoxNumber;
+            int secondNumber;
+            int thirdnumber;
+            int answer;
+
+            firstTextBoxNumber = int.Parse(textBox1.Text);
+            secondNumber = int.Parse(textBox2.Text);
+            thirdnumber = int.Parse(textBox3.Text);
+
+            //Exercise Logic - Uncoment Logic based on question
+
+           //answer = (firstTextBoxNumber + secondNumber) - thirdnumber;
+           //answer = (firstTextBoxNumber - secondNumber) * thirdnumber;
+           //answer = firstTextBoxNumber * (secondNumber - thirdnumber);
+           answer = (firstTextBoxNumber / secondNumber) + thirdnumber;
+
+            MessageBox.Show(answer.ToString(), "Text Box Number", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            MessageBox.Show("Check out my text box logic", "Stepping up", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
